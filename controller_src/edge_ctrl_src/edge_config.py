eddd40592b820f487595186ea46abdd4a6c81dec
zhouyou-gu <guzhouyou@gmail.com>
2023-03-23 19:40:18 +1100

    Update edge_config.py

--- controller_src/edge_ctrl_src/edge_config.py
+++ controller_src/edge_ctrl_src/edge_config.py
@@ -13,14 +13,14 @@ from edge_ctrl_src.edge import PC_ACCESS_CONFIG, EDGE_ACCESS_CONFIG
 ue_config_list = []
 # SSH access to edge
 ue1 = PC_ACCESS_CONFIG(name='ue1', ip='IP-TO-UE1', port='PORT-TO-UE1', username='USERNAME-OF-UE1', password='PASSWORD-TO-UE1',
-                       working_dir='$HOME/drl-5g-scheduler/')
+                       working_dir='~/drl-5g-scheduler/')
 ue2 = PC_ACCESS_CONFIG(name='ue2', ip='IP-TO-UE2', port='PORT-TO-UE2', username='USERNAME-OF-UE2', password='PASSWORD-TO-UE2',
-                       working_dir='$HOME/drl-5g-scheduler/')
+                       working_dir='~/drl-5g-scheduler/')
 ue_config_list.append(ue1)
 ue_config_list.append(ue2)
 
 enb = PC_ACCESS_CONFIG(name='enb', ip='IP-TO-ENB', port='PORT-TO-ENB', username='USERNAME-OF-ENB', password='PASSWORD-TO-ENB',
-                       working_dir='$HOME/drl-5g-scheduler/')
+                       working_dir='~/drl-5g-scheduler/')
 epc = enb
 
 edge_config = EDGE_ACCESS_CONFIG(epc_config=epc, enb_config=enb, ue_config_list=ue_config_list)
